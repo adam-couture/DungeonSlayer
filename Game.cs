61894fe7ccd63c9cab9c7d060f50f4516459c8f0
Adam Couture <acouture@iti4dmv.com>
2021-08-18 16:32:10 -0400

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DungeonSlayer
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome Dungeon Slayer!!!");
+
+            PlayerCharacter slayer = new PlayerCharacter();
+
+            Console.WriteLine("Press \"Enter\" to move");
+
+            bool ContinuePlay = true;
+
+            while (ContinuePlay = true)
+            {
+
+
+                if (slayer.Health != 0)
+                {
+                    
+                }
+                Console.WriteLine("Your current health is" + slayer.Health);
+                Console.WriteLine("Your current loot is" + slayer.Loot);
+
+                Console.WriteLine("Would you like to move again or end? (move/end)");
+                string answer = Console.ReadLine();
+                if (answer == "end")
+                {
+                    break;
+                }
+                if (answer == "move")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Answer must be \"move\" or \"end\"");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ PlayerCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonSlayer
+{
+    class PlayerCharacter
+    {
+        
+        public int Health { get; set; }
+        public int Loot { get; set; }
+        public PlayerCharacter( int health = 100, int loot = 0)
+        {
+            Health = health;
+            Loot = loot;
+        }
+        //public static void MoveCharacter()
+        //{
+
+        //}
+    }
+}
